71cabdd9e3cbebd0ae08766f4f92619646d864f9
aschultz10 <77344570+aschultz10@users.noreply.github.com>
2021-05-31 17:43:21 -0400

    up to date

--- static/js/app.js
+++ static/js/app.js
@@ -2,139 +2,127 @@
 
 var url = 'http://127.0.0.1:5000/data'
 // using this portion to test code
-// d3.json(url).then(function(data) {
-  // console.log(data.Country[0])
-  // var country = 'Aruba'
-  // var sample = data.Country
-  // console.log(sample.name)
-  // var filtersample = sample.filter(sampleobject => sampleobject.name == country);
-  // console.log(filtersample)
-// });
-//   var test = data.Country;
-//   test.forEach((country)=>{
-//     console.log(country.id)
-//     // AID.append("name")
-//     // .text()
-//   })
-// });
-
-var url = 'http://127.0.0.1:5000/data'
+d3.json(url).then(function(data) {
+});
 
 
 function dropdown(){
+  var url = 'http://127.0.0.1:5000/data'
   var ID = d3.select("#selDataset");
   d3.json(url).then(function(data){
-
-      console.log(data);
       var countries = data.Country;
       countries.forEach((country)=>{
           ID.append("option")
-          .text(country.name)
-          // .property("value",country.name)
+          .text(country.Name)
       })
+      var country1 = countries[0];
+      buildMetadata(country1.Name);
+      buildCharts(country1.Name);
   })
 }
 dropdown()
 
 function optionChanged(newCountry){
-  buildMetadata(newCountry)
-  //buildCharts(newCountry)
-  plotCharts(newCountry);
-  // gguage_plot(newCountry)
+  buildMetadata(newCountry);
+  buildCharts(newCountry);
 }
-
 
 function buildMetadata(country){
   d3.json(url).then(function(data){
     var countryInfo = data.Country
-    var filterdata = countryInfo.filter(sampleobject => sampleobject.name == country);
-    var result = filterdata[0];
+    var filterdata = countryInfo.filter(sampleobject => sampleobject.Name == country)[0];
+    var result = filterdata;
     var sampleData = d3.select("#sample-metadata");
     sampleData.html("");
     Object.entries(result).forEach(function([key, value]){
-      // fix this if condition
-      if(result) {
-        var row = sampleData.append("p");
-        row.text(`${key}: ${value}`)
-      }
+      var row = sampleData.append("p");
+      row.text(`${key}: ${value}`)
     })
   })
 
 }
 
 // ToDo Wite code for charts here:
-function plotCharts(country){
-  var usedata = [];
+function buildCharts(sample) {
+  // Use d3.json to get data
   d3.json(url).then(function(data) {
-    var countryInfo = data.Country
-    var filterdata = countryInfo.filter(sampleobject => sampleobject.name == country);
-    
-    x_variable_keys = ["RT_EducationScore","RT_FoodScore","RT_HealthScore", "RT_HousingScore","RT_WorkScore"];
+      var countryInfo = data.Country;
+      // console.log(countryInfo)
+      var filterdata = countryInfo.filter(sampleobject => sampleobject.Name == sample)[0];
+      var result = filterdata;
+      // console.log(result.GDP)
+  
+      var GDP = result.GDP / 100;
+      var Education = result.Education;
+      var Food = result.Food;
+      var Health = result.Health;
+      var Housing = result.Housing;
+      var Work = result.Work;
+      var QalityOfLife = result.QualityOfLife;
+      var yAxis = ['GDP','Education','Food','Health','Work','Housing']
+      var yAxis2 = ['Education','Food','Health','Work','Housing']
+      var xAxis2 = [Education,Food,Health,Work,Housing,]
+      var xAxis = [GDP,Education,Food,Health,Work,Housing,]
+      // console.log(QalityOfLife)
 
-    // Loop through the countries and push keys to the chart
-    for(i=0; i < x_variable_keys.length; i++){
-      usedata.push({label: x_variable_keys[i], value: filterdata[0][x_variable_keys[i]] })
-    }
+      //barchart Horizontal:
+      var barchart = [{
+        y: yAxis,
+        x: xAxis,
+        type: 'bar',
+        orientation: 'h'
+      }];
 
-    usedata["columns"] = x_variable_keys
+      var barlayout = {
+        title: "Top RIghts Scores"
+      }
 
-    console.log("Usedata", usedata, x_variable_keys.length);
-    plotBar(usedata);
+      Plotly.newPlot('bar', barchart, barlayout)
 
-});
+      // guage_plot
+      var gauge_plot = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: QalityOfLife,
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+        axis: { range: [null, 100] },
+        bar: {color: '7EA397'},
+        bgcolor: "white"        
+                }
 
-}
+      }];
+      var gaugelayout = {
+        title: "Quality of Life Score",
+        paper_bgcolor: 'white'
+      }
 
-function plotBar(data){
+      Plotly.newPlot('gauge', gauge_plot, gaugelayout);
 
-d3.select("#bar-chart-svg").html("");
+      var bubbledata = [{
+        x: yAxis2,
+        y: xAxis2,
+        text: yAxis2,
+        mode: 'markers',
+        marker: {
+          size: xAxis2,
+          color: xAxis,
+          coloscale: "Earth"
+        }
+      }];
 
-// Dimensions and Margins
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+      var bubblelayout = {
+        title: "BubbleChart",
+        xaxis: {
+          title: "Scores vs GDP"
+        }
+      }
 
-//Append SVG
-var svg = d3.select("#bar-chart-svg")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-// make X axis then Append
-
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.label; }))
-  .padding(0.2);
-
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-// same thing with Y axis
-
-var y = d3.scaleLinear()
-  .range([ height, 0])
-  .domain([0, 100])
-svg.append("g")
-  .call(d3.axisLeft(y));
+      Plotly.newPlot('bubble', bubbledata, bubblelayout)
+      
+  })    
 
 
-// Append bars to the chart
-svg.selectAll("myBar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.label); })
-    .attr("y", function(d) { return y(d.value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.value); })
-    .attr("fill", "blue")
 
+        
 }
