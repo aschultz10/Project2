dbb5416cd3232c5400f5ce27db33041c69d0decb
Jennifer Kirschnick Duffy <jenkirschnick@gmail.com>
2021-08-23 13:47:10 -0400

    Refreshing files

--- static/plots_jen.js
+++ static/plots_jen.js
@@ -3,67 +3,65 @@
 //-----------------------------------------
 // define drop down menu:
 
-var url = 'http://127.0.0.1:5000/data'
-// using this portion to test code
-// d3.json(url).then(function(data) {
-  // console.log(data.Country[0])
-  // var country = 'Aruba'
-  // var sample = data.Country
-  // console.log(sample.name)
-  // var filtersample = sample.filter(sampleobject => sampleobject.name == country);
-  // console.log(filtersample)
-// });
-//   var test = data.Country;
-//   test.forEach((country)=>{
-//     console.log(country.id)
-//     // AID.append("name")
-//     // .text()
+// var url = 'http://127.0.0.1:5000/data'
+// // using this portion to test code
+// // d3.json(url).then(function(data) {
+//   // console.log(data.Country[0])
+//   // var country = 'Aruba'
+//   // var sample = data.Country
+//   // console.log(sample.name)
+//   // var filtersample = sample.filter(sampleobject => sampleobject.name == country);
+//   // console.log(filtersample)
+// // });
+// //   var test = data.Country;
+// //   test.forEach((country)=>{
+// //     console.log(country.id)
+// //     // AID.append("name")
+// //     // .text()
+// //   })
+// // });
+
+// function dropdown(){
+//   var url = 'http://127.0.0.1:5000/data'
+//   var ID = d3.select("#selDataset");
+//   d3.json(url).then(function(data){
+//       var countries = data.Country;
+//       countries.forEach((country)=>{
+//           ID.append("option")
+//           .text(country.name)
+//           // .property("value",country.name)
+//       })
+//       var country1 = countries[0];
+//       buildMetadata(country1);
+//       // buildCharts(country1);
 //   })
-// });
+// }
+// dropdown()
 
-function dropdown(){
-  var url = 'http://127.0.0.1:5000/data'
-  var ID = d3.select("#selDataset");
-  d3.json(url).then(function(data){
-      var countries = data.Country;
-      countries.forEach((country)=>{
-          ID.append("option")
-          .text(country.name)
-          // .property("value",country.name)
-      })
-      var country1 = countries[0];
-      buildMetadata(country1);
-      // buildCharts(country1);
-  })
-}
-dropdown()
+// function optionChanged(newCountry){
+//   buildMetadata(newCountry)
+//   // buildCharts(newCountry)
+//   // gguage_plot(newCountry)
+// }
 
-function optionChanged(newCountry){
-  buildMetadata(newCountry)
-  // buildCharts(newCountry)
-  // gguage_plot(newCountry)
-}
+// function buildMetadata(country){
+//   d3.json(url).then(function(data){
+//     var countryInfo = data.Country
+//     var filterdata = countryInfo.filter(sampleobject => sampleobject.name == country);
+//     var result = filterdata[0];
+//     var sampleData = d3.select("#sample-metadata");
+//     sampleData.html("");
+//     Object.entries(result).forEach(function([key, value]){
+//       // fix this if condition
+//       if(result) {
+//         var row = sampleData.append("p");
+//         row.text(`${key}: ${value}`)
+//       }
+//     })
+//   })
+// }
 
-
-function buildMetadata(country){
-  d3.json(url).then(function(data){
-    var countryInfo = data.Country
-    var filterdata = countryInfo.filter(sampleobject => sampleobject.name == country);
-    var result = filterdata[0];
-    var sampleData = d3.select("#sample-metadata");
-    sampleData.html("");
-    Object.entries(result).forEach(function([key, value]){
-      // fix this if condition
-      if(result) {
-        var row = sampleData.append("p");
-        row.text(`${key}: ${value}`)
-      }
-    })
-  })
-
-}
-
-// ToDo Wite code for charts here:
+// ToDo Write code for charts here:
 
 // function buildCharts(sample) {
 //   // Use d3.json to get data
@@ -77,170 +75,219 @@ function buildMetadata(country){
 //       console.log(metadata)
 //   })
 // }
+//Extract info from csv file(s)
+
+import Papa from "https://cdn.skypack.dev/papaparse@5.3.0";
+let fileInput = document.getElementById("income").files[0]Papa.parse(file,
+    {
+        complete:function(result) {
+            console.log(result.data)
+        }
+    });
+
+// Key data by field name instead of index/position
+// var results = Papa.parse(csv, {
+// 	header: true
+// });
+
+
 
 
 //----------------------------------------------
 //  CODE BELOW IS TAKEN FROM PLOTLY-CHALLENGE
 //----------------------------------------------
 
-// Create the function that gets the data and creates the plots for the id 
-function getPlot(id) {
+// // Create the function that gets the data and creates the plots for the id 
+// function getPlot(id) {
     
-    // Retrieve data from the json file
-    d3.json("../data/samples.json").then((data)=> {
-        //console.log(data)
-    //});
-        //var wfreq = data.metadata.map(d => d.wfreq)
-        //console.log(`Washing Freq: ${wfreq}`)
-
-        // Filter sample values by id 
-        var samples = data.samples.filter(s => s.id.toString() === id)[0];
-
-        console.log(samples);
-
-        //Retrieve top 10 sample values to plot and reverse for Plotly
-        var sampleValues = samples.sample_values.slice(0, 10).reverse();
-
-        // Retrieve top 10 otu ids for the plot
-        var idValues = (samples.otu_ids.slice(0, 10)).reverse();
+//     // Retrieve data from the json file
+//     //WHAT TYPE OF FILE DO WE IMPORT TO GET THE DATA TO PLOT??
+//     d3.json("../data/samples.json").then((data)=> {
+//         //console.log(data)
+//     //});
         
-        // Retrieve the otu ids to the desired form for the plot
-        var idOtu = idValues.map(d => "OTU " + d)
+//         // Filter country values by name 
+//         var countryx = data.samples.filter(s => s.id.toString() === id)[0];
 
-        console.log(`OTU IDS: ${idOtu}`)
+//         //console.log(samples);
 
-        // Retrieve the top 10 labels for the plot
-        var labels = samples.otu_labels.slice(0, 10);
+//         //Retrieve top 10 sample values to plot and reverse for Plotly
+//         var sampleValues = samples.sample_values.slice(0, 10).reverse();
 
-        console.log(`Sample Values: ${sampleValues}`)
-        console.log(`Id Values: ${idValues}`)
-
+//         // Retrieve top 10 otu ids for the plot
+//         var idValues = (samples.otu_ids.slice(0, 10)).reverse();
         
-        // Create trace variable for the plot
-        var trace = {
-            x: sampleValues,
-            y: idOtu,
-            text: labels,
-            type:"bar",
-            orientation: "h",
-        };
+//         // Retrieve the otu ids to the desired form for the plot
+//         var idOtu = idValues.map(d => "OTU " + d)
 
-        // Create data variable
-        var data = [trace];
+//         console.log(`OTU IDS: ${idOtu}`)
 
-        // Create layout variable to set plots layout
-        var layout = {
-            title: "Top 10 OTU",
-            yaxis:{
-                tickmode:"linear",
-            },
-            margin: {
-                l: 100,
-                r: 100,
-                t: 30,
-                b: 20
-            }
-        };
+//         // Retrieve the top 10 labels for the plot
+//         var labels = samples.otu_labels.slice(0, 10);
 
-        // Create the bar plot
-        Plotly.newPlot("bar", data, layout);
+//         console.log(`Sample Values: ${sampleValues}`)
+//         console.log(`Id Values: ${idValues}`)
 
-        //console.log(`ID: ${samples.otu_ids}`)
+//  //example of multi trace       
+// //trace1 for Greek data
+// var trace1 = {
+//     //"pair" and "greekSearchResults" are from the data.js file
+//     x: data.map(row => row.pair),
+//     y: data.map(row => row.greekSearchResults),
+//     text: data.map(row => row.greekName),
+//     name: "Greek",
+//     type: "bar"
+// };
+
+// //trace2 for Roman data
+// var trace2 = {
+//     x: data.map(row => row.pair),
+//     y: data.map(row => row.romanSearchResults),
+//     text: data.map(row => row.romanName),
+//     name: "Roman",
+//     type: "bar"
+// };
+
+// //Combine the traces
+// var traceData = [trace1, trace2];
+
+// //Apply the "group barmode" to the layout
+// var layout = {
+//     title: "Greek vs. Roman gods searxh results",
+//     barmode: "group"
+// };
+
+// //Render the plot to the div tag with id "plot"
+// Plotly.newPlot("plot", traceData, layout);
+
+
+
+
+//         // Create trace variable for the plot
+//         var trace = {
+//             x: sampleValues,
+//             y: idOtu,
+//             text: labels,
+//             type:"bar",
+//             orientation: "h",
+//         };
+
+//         // Create data variable
+//         var data = [trace];
+
+//         // Create layout variable to set plots layout
+//         var layout = {
+//             title: "Top 10 OTU",
+//             yaxis:{
+//                 tickmode:"linear",
+//             },
+//             margin: {
+//                 l: 100,
+//                 r: 100,
+//                 t: 30,
+//                 b: 20
+//             }
+//         };
+
+//         // Create the bar plot
+//         Plotly.newPlot("bar", data, layout);
+
+//         //console.log(`ID: ${samples.otu_ids}`)
         
-        // Create the trace for the bubble chart
-        var trace1 = {
-            x: samples.otu_ids,
-            y: samples.sample_values,
-            mode: "markers",
-            marker: {
-                size: samples.sample_values,
-                color: samples.otu_ids
-            },
-            text: samples.otu_labels
+//         // Create the trace for the bubble chart
+//         var trace1 = {
+//             x: samples.otu_ids,
+//             y: samples.sample_values,
+//             mode: "markers",
+//             marker: {
+//                 size: samples.sample_values,
+//                 color: samples.otu_ids
+//             },
+//             text: samples.otu_labels
 
-        };
+//         };
 
-        // Layout for the bubble plot
-        var layout = {
-            xaxis:{title: "OTU ID"},
-            height: 600,
-            width: 1300
-        };
+//         // Layout for the bubble plot
+//         var layout = {
+//             xaxis:{title: "OTU ID"},
+//             height: 600,
+//             width: 1300
+//         };
 
-        // Create the data variable 
-        var data1 = [trace1];
+//         // Create the data variable 
+//         var data1 = [trace1];
 
-        // Create bubble plot
-        Plotly.newPlot("bubble", data1, layout); 
+//         // Create bubble plot
+//         Plotly.newPlot("bubble", data1, layout); 
 
-        // Create pie chart
-        var tracePie = {
-            labels: idOtu,
-            values:sampleValues,
-            type:"pie"
-        }
+//         // Create pie chart
+//         var tracePie = {
+//             labels: idOtu,
+//             values:sampleValues,
+//             type:"pie"
+//         }
 
-        var data = [tracePie]
+//         var data = [tracePie]
         
-        var layout = {
-                title: "Top 10 OTU",
-        };
+//         var layout = {
+//                 title: "Top 10 OTU",
+//         };
             
-        Plotly.newPlot("pie", data, layout)
+//         Plotly.newPlot("pie", data, layout)
 
-    });    
-}
+//     });    
+// }
     
-// Create function to get the necessary data
-function getInfo(id) {
-    //Read json file to get data
-    d3.json("../data/samples.json").then((data)=> {
+// // Create function to get the necessary data
+// function getInfo(id) {
+//     //Read json file to get data
+//     d3.json("../data/samples.json").then((data)=> {
         
-        // Retrieve metadata info for the demographic panel
-        var metadata = data.metadata;
+//         // Retrieve metadata info for the demographic panel
+//         var metadata = data.metadata;
 
-        console.log(metadata)
+//         console.log(metadata)
 
-        // Filter metadata info by id
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+//         // Filter metadata info by id
+//         var result = metadata.filter(meta => meta.id.toString() === id)[0];
 
-        // Select demographic panel to put data
-        var demographicInfo = d3.select("#sample-metadata");
+//         // Select demographic panel to put data
+//         var demographicInfo = d3.select("#sample-metadata");
         
-        // Empty the demographic info panel each time before getting new id info
-        demographicInfo.html("");
+//         // Empty the demographic info panel each time before getting new id info
+//         demographicInfo.html("");
 
-        // Get necessary demographic data for the id and append the info to the panel
-        Object.entries(result).forEach((key) => {   
-                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
-        });
-    });
-}
+//         // Get necessary demographic data for the id and append the info to the panel
+//         Object.entries(result).forEach((key) => {   
+//                 demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+//         });
+//     });
+// }
 
-// Create the function for the change event
-function optionChanged(id) {
-    getPlot(id);
-    getInfo(id);
-}
+// // Create the function for the change event
+// function optionChanged(id) {
+//     getPlot(id);
+//     getInfo(id);
+// }
 
-// Create the function for the initial data rendering
-function init() {
-    // Select dropdown menu 
-    var dropdown = d3.select("#selDataset");
+// // Create the function for the initial data rendering
+// function init() {
+//     // Select dropdown menu 
+//     var dropdown = d3.select("#selDataset");
 
-    // Read data 
-    d3.json("../data/samples.json").then((data)=> {
-        console.log(data)
+//     // Read data 
+//     d3.json("../data/samples.json").then((data)=> {
+//         console.log(data)
 
-        // Id data to the dropdwown menu
-        data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
-        });
+//         // Id data to the dropdwown menu
+//         data.names.forEach(function(name) {
+//             dropdown.append("option").text(name).property("value");
+//         });
 
-        // Call the functions to display the data and the plots to the page
-        getPlot(data.names[0]);
-        getInfo(data.names[0]);
-    });
-}
+//         // Call the functions to display the data and the plots to the page
+//         getPlot(data.names[0]);
+//         getInfo(data.names[0]);
+//     });
+// }
 
-init();
+// init();
